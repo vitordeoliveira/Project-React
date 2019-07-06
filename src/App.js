aa867e21915db847ae0e007fb46dd5aecadc4782
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2019-07-06 10:11:40 -0300

    increment module

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import "./App.css";
 function App() {
   const [isRed, setRed] = useState(false);
   const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+    setRed(!isRed);
+  };
   return (
     <div className="App">
-      <Tweet name="Vitor" />
-      <Tweet name="Aline" />
-      <Tweet name="Odir" />
-      <Tweet name="Marcia" />
+      <h1 className={isRed ? "red" : ""}>Change My Color</h1>
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
     </div>
   );
 }
